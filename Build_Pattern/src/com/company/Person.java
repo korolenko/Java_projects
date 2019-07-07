51191b0964e4964c71d495fa2c12cf17587daee8
Korolenko Roman <korolenko.as1204@asugubkin.ru>
2019-07-07 17:14:05 +0300

    Build pattern in Java

---
+++ Build_Pattern/src/com/company/Person.java
@@ -0,0 +1,60 @@
+package com.company;
+
+//демонстрация шаблона проектирования Builder
+//создаем класс Person
+public class Person {
+    private String Name;
+    private String SurName;
+    private int Age;
+
+    //создаем геттеры, сеттеры
+    public String getName() {
+        System.out.println("Person.Name: " + this.Name);
+        return Name;
+    }
+
+    private void setName(String name) {
+        Name = name;
+    }
+
+    public String getSurName() {
+        System.out.println("Person.SurName: " + this.SurName);
+        return SurName;
+    }
+
+    private void setSurName(String surName) {
+        SurName = surName;
+    }
+
+    public int getAge() {
+        System.out.println("Person.Age: " + this.Age);
+        return Age;
+    }
+
+    private void setAge(int age) {
+        Age = age;
+    }
+    //создаем класс, в который вынесено заполнение свойств класса Person
+    static class personBuilder {
+        private Person person;
+
+        personBuilder(){
+            person = new Person();
+        }
+        public personBuilder withName(String name){
+            this.person.setName(name);
+            return this;
+        }
+        public personBuilder withSurname(String surname){
+            this.person.setSurName(surname);
+            return this;
+        }
+        public personBuilder whithAge(int age){
+            this.person.setAge(age);
+            return this;
+        }
+        public Person getPerson(){
+            return this.person;
+        }
+    }
+}
